119e36fdcf209c0ab7c3102fbdfb4f261d26ebd4
Alexandre Kelifa <alexandre.kelifa@hotmail.com>
2021-12-17 11:22:34 +0100

    funny 404

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,5 +1,5 @@
 module.exports = {
     error404: (_, response) => {
-        response.status(404).json({error: `Erreur 404: la page n'a pas été trouvée!`});
+        response.status(404).json({error: `Yuh galang, man! Nuttin gwaan there! Dis a waah 404 erra!!!`});
     }
 } 
